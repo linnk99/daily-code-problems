0d81b5e8a3d5ac0dd705c6fbdd6d3aedf9212129
Hector Vega <linnk99@gmail.com>
2019-12-14 16:05:56 -0500

    Python code for the problem is working

--- 12-14-2019/sum-in-list.py
+++ 12-14-2019/sum-in-list.py
@@ -7,11 +7,13 @@ def generate_list(numbers_list, list_length):
     print(numbers_list)
             
 def check_sum(numbers_list, number_to_check):
-    for i in (numbers_list):
+    for i in range(len(numbers_list)):
         if number_to_check - numbers_list[i] in numbers_list:
             print('The number {} is a sum of two numbers in the list'.format(number_to_check))
+            break
         else:
             print('The number {} is NOT a sum of two numbers in the list'.format(number_to_check))
+            break
         
 
 def run():
